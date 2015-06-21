e70d8016c6605ec7b35855cf2446e6d83fd8e5f3
Marcus E. Turnbo <marcus@emarcus.com>
2015-06-21 16:56:04 -0500

    Updated UI.  Added .gitignore

--- ui.R
+++ ui.R
@@ -8,11 +8,18 @@ shinyUI(pageWithSidebar(
     sliderInput('avgExps', 'Set Number of Average Exponentials',value = 40, min = 20, max = 100, step = 5,)
   ),
   mainPanel(
-    p("Use this tool to explore exponential distribution and compare it with the Central Limit Theorem.  The exponential distribution can be simulated in R with rexp(n, lambda) where lambda is the rate parameter.   For the simulation choose a lambda value and number of exponentials with the sliders on the left."),
-    p("Confidence Interval:"),
-    textOutput('confInterval'),
+    p("Use this tool to explore exponential distribution and compare it with the Central Limit Theorem.  
+      The exponential distribution can be simulated in R with rexp(n, lambda) where lambda is the rate parameter.   
+      For the simulation choose a lambda value and number of exponentials with the sliders on the left."),
+    tags$hr(),
+    p("Histogram of the distribution with the theoretical mean in blue and the sample mean in red."),
     plotOutput('expDist'),
-    plotOutput('avgExpDist')
+    tags$hr(),
+    p("Histogram of the distribution of averages with the theoretical mean in blue and the sample mean in red."),
+    plotOutput('avgExpDist'),
+    tags$hr(),
+    p("Confidence Interval:"),
+    textOutput('confInterval')
   )
 ))
 
